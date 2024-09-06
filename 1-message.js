a8d7e40cfffcdca3dc87d9c71d2e232a02a539e5
Chamak Saha <chamaksaha58@gmail.com>
2024-09-06 23:44:26 +0600

    added task: messege repeated for 60 times.

---
+++ 1-message.js
@@ -0,0 +1,17 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+
+let personNo = 1;
+let message = "I will invest at least 6 hrs every single day for next 60 days!";
+console.log(message);
+
+while(personNo <= 60){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    personNo++;
+}
